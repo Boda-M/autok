c7a47ed87d978d4c50e9737a14e1cf85eb8cac9b
Boda-M <mateboda05@gmail.com>
2024-10-14 14:16:37 +0200

    progress bar

--- app/Console/Commands/get_makers.php
+++ app/Console/Commands/get_makers.php
@@ -4,6 +4,8 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Models\Maker;
+use function Laravel\Prompts\progress;
+
 
 class get_makers extends Command
 {
@@ -24,25 +26,33 @@ class get_makers extends Command
     /**
      * Execute the console command.
      */
+    private $progress;
+
     public function handle()
     {
+        $this->progress = progress(label: 'Reading file (1/3)', steps: 100);
+        $this->progress->start();
         $data = $this->getCsvData('car-db.csv');
         $makers = $this->getMakers($data);
-        
+        $this->progress->label = 'Uploading to DB (3/3)';
         for ($i=0; $i < count($makers); $i++) { 
             $entity = new Maker();
             $entity->name = $makers[$i];
             $entity->save();
+            if($i % floor(count($makers)/33) == 0)$this->progress->advance();
         }
+        $this->progress->finish();
     }
 
     private function getMakers($data){
         $makers = [];
+        $this->progress->label = 'Getting makers (2/3)';
         for ($i=0; $i < count($data); $i++) {
             $maker = $data[$i][1]; 
             if(array_search($maker, $makers) === false){
                 $makers[] = $maker;
             }
+            if($i % floor(count($data)/33) == 0)$this->progress->advance();
         }
         return $makers;
     }
@@ -50,9 +60,12 @@ class get_makers extends Command
     private function getCsvData($filename): array{
         $file = fopen($filename, 'r');
         $result = [];
+        $i = 0;
+        $totalLines = count(file("car-db.csv"));
         while(!feof($file)){
             $line = fgetcsv($file);
             $result[] = $line;
+            if($i++ % floor($totalLines/33)  == 0)$this->progress->advance();
         }
         fclose($file);
 
--- database/migrations/2024_10_14_121611_models.php
+++ database/migrations/2024_10_14_121611_models.php
@@ -6,13 +6,12 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
-        Schema::create('makers', function (Blueprint $table)
-        {
-            $table->id();
-            $table->string('name');
-        });
+        //
     }
 
     /**
@@ -20,6 +19,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::drop('makers');
+        //
     }
 };
