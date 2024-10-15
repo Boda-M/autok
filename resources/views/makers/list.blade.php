4fb8bfbcfbb86adc75a05a62e54eda25801a48e9
Boda-M <mateboda05@gmail.com>
2024-10-15 14:03:23 +0200

    Kilistáz valamit
    De milyen áron?

--- resources/views/makers/list.blade.php
+++ resources/views/makers/list.blade.php
@@ -1,3 +1,19 @@
-<div>
-    <!-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk -->
-</div>
+@extends('layouts.app')
+
+<table>
+    <thead>
+        <tr>
+            <th>#</th>
+            <th class="search-field">Gyártó</th>
+            
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($entities as $entity)
+            <tr>
+                <td id={{$entity->id}}>{{$entity->id}}</td>
+                <td>{{$entity->name}}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
